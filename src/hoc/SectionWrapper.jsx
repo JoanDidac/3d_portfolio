159e6216fe9ed7df42a2b65c45838e2fa4b1e7db
Joan D <joandidachr@gmail.comm>
2024-07-11 14:42:53 +0200

    Adds About section and tilted cards + SectionWrapper for all sections as high order comp (stable version)

---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { motion } from 'framer-motion';
+import {styles} from '../styles';
+import { staggerContainer } from "../utils/motion";
+
+
+//kind of a higher order component ot keep all content straight and centered with same paddings and margins
+
+const SectionWrapper = ( Component, idName ) => 
+
+function HOC() {
+    return(
+        <motion.section
+        variants={{staggerContainer}}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }}
+        className={` ${styles.padding} max-w-7xl mx:auto relative z-0`}
+        >
+
+        <span className="hash-span" id= {idName}>
+          &nbsp;
+            
+        </span>
+        
+            <Component />
+        </motion.section>
+    )
+}
+
+
+export default SectionWrapper
