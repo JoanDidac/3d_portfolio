ea3b3b9b97672048640f44f3639c64b600df478c
Joan D <joandidachr@gmail.comm>
2024-07-12 15:55:25 +0200

    Adds contact form (not functional yet) + slideIn imporved + placeholders styled (Stable Version)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -26,7 +26,7 @@ const Contact = () => {
       variants={slideIn('left', "tween", 0.2, 1)}
       className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
       >
-      <p className={styles.sectionSubText}>Get in touch</p>
+      <p className={styles.sectionSubText}>Get in touch</p> 
       <h3 className={styles.sectionHeadText}>Contact.</h3>
 
         <form  
@@ -39,15 +39,41 @@ const Contact = () => {
             <input
             type="text"
             name="name"
-            value={form.name}
+            value={form.name} 
             onChange={handleChange}
-            placeholder="What's your name?"
+            placeholder="What's your name human?"
             className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rouned-lg outline-none border-none font-medium"
- 
-
             />
           </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Email</span>
+            <input
+            type="email"
+            name="email"
+            value={form.email}
+            onChange={handleChange}
+            placeholder="What's your email?"
+            className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rouned-lg outline-none border-none font-medium"
+            />
+          </label>
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Your Message</span>
+            <textarea
+            rows='7'
+            name="message"
+            value={form.message}
+            onChange={handleChange}
+            placeholder="What do you want to say?"
+            className="bg-tertiary py-4 px-6 placeholder:text-secondary text-white rouned-lg outline-none border-none font-medium"
+            />
+          </label>
+           <button
+           type="submit"
+           className="bg-tertiary py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-xl"
+           >
+           {loading ? 'Sending...' : "Send"}
 
+           </button>
         </form>
 
 
