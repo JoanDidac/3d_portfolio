a6ed1df4c0575509b7d412ff73d73d9261640f6d
Joan D <joandidachr@gmail.comm>
2024-07-17 15:11:35 +0200

    Adds and corrects Experience text and years in TimeLine element, fixes all icons and adds correct order, Stable Version, not merged to main

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import typeform from "./company/typeform-logo.png";
+import king from "./company/King-logo.png";
+import amalgama from "./company/amalgama-digital logo.png";
+import ironhack from "./company/ironhack logo.jpeg";
 
 import skypulse from "./Sky Pulse Thumbnail.png";
 import foodfolio from "./Food Folio thumbnail.png";
@@ -52,10 +52,10 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  typeform,
+  king,
+  amalgama,
+  ironhack,
   skypulse,
   foodfolio,
   ninja,
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -19,7 +19,7 @@ const ExperienceCard = ({experience}) => (
     <img 
       src={experience.icon}
       alt={experience.company_name}
-      className=" w-[60%] h-[60%] object-contain "
+      className="bg-slate-50 rounded-full  w-full h-full object-contain "
       /> 
   </div>
   }
