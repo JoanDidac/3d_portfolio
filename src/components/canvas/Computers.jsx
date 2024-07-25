e29b83a4f41845480368bfdf4acc7b2290ed888c
Joan D <joandidachr@gmail.comm>
2024-07-25 20:00:38 +0200

    Adds on X and Y Axis for Computer jsx + iphone XR responsive too, Stable Version, not merged to main

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -60,9 +60,13 @@ return () => {
     >
     <Suspense fallback={<CanvasLoader/>}>
     <OrbitControls
-    enableZoom={false}
-    maxPolarAngle={Math.PI / 2} 
-    minPolarAngle={Math.PI / 2} 
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 1.5} // Allow some vertical movement
+          minPolarAngle={Math.PI / 3}   // Allow some vertical movement
+          enableRotate={true}           // Allow rotation around the object
+          enablePan={true}              // Allow panning
+          enableDamping={true}          // Enable smooth damping
+          dampingFactor={0.1}           // Set damping factor
     />
       <Computers isMobile={isMobile}/>
     </Suspense>
