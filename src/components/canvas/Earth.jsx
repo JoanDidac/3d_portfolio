4a74e653a49f241d5e467d363fdcd5c4a7887bbe
Joan D <joandidachr@gmail.comm>
2024-09-01 01:19:40 +0200

    Fixes bufferGeometry position has NaN values, Stable Verision not merged to main

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react"
-import { Canvas } from "react-three-fiber";
+import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from '../Loader';
